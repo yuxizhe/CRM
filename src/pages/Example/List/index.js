9dd413285384d651351b32dbdc9fc1259fc1032d
yuxz <yuxz@xueqiu.com>
2018-12-12 15:16:52 +0800

    list example

--- src/pages/Example/List/index.js
+++ src/pages/Example/List/index.js
@@ -1,5 +1,8 @@
-import { Table, Icon, Switch, Radio, Form, Divider, Card } from "antd";
+import { Table, Icon, Select, Button, Input, Form, Divider, Card } from "antd";
+import { observer, inject } from "mobx-react";
+import store from "./store";
 
+const Search = Input.Search;
 const FormItem = Form.Item;
 
 const columns = [
@@ -39,24 +42,9 @@ const columns = [
   }
 ];
 
-const data = [];
-for (let i = 1; i <= 10; i++) {
-  data.push({
-    key: i,
-    name: "John Brown",
-    age: `${i}2`,
-    address: `New York No. ${i} Lake Park`,
-    description: `My name is John Brown, I am ${i}2 years old, living in New York No. ${i} Lake Park.`
-  });
-}
-
 const expandedRowRender = record => <p>{record.description}</p>;
-const title = () => "Here is title";
-const showHeader = true;
-const footer = () => "Here is footer";
-const scroll = { y: 240 };
 const pagination = { position: "bottom" };
-
+@observer
 class UserList extends React.Component {
   state = {
     bordered: false,
@@ -64,59 +52,23 @@ class UserList extends React.Component {
     pagination,
     size: "default",
     expandedRowRender,
-    title: undefined,
-    showHeader,
-    footer,
+    showHeader: true,
     rowSelection: {},
     scroll: undefined,
     hasData: true
   };
+  store = store;
 
-  handleToggle = prop => enable => {
-    this.setState({ [prop]: enable });
+  componentDidMount() {
+    this.store.getList();
+  }
+
+  onStatusSelectChange = value => {
+    this.store.getList(value);
   };
-
-  handleSizeChange = e => {
-    this.setState({ size: e.target.value });
+  handleButtonClick = value => {
+    this.store.clickButton(value);
   };
-
-  handleExpandChange = enable => {
-    this.setState({
-      expandedRowRender: enable ? expandedRowRender : undefined
-    });
-  };
-
-  handleTitleChange = enable => {
-    this.setState({ title: enable ? title : undefined });
-  };
-
-  handleHeaderChange = enable => {
-    this.setState({ showHeader: enable ? showHeader : false });
-  };
-
-  handleFooterChange = enable => {
-    this.setState({ footer: enable ? footer : undefined });
-  };
-
-  handleRowSelectionChange = enable => {
-    this.setState({ rowSelection: enable ? {} : undefined });
-  };
-
-  handleScollChange = enable => {
-    this.setState({ scroll: enable ? scroll : undefined });
-  };
-
-  handleDataChange = hasData => {
-    this.setState({ hasData });
-  };
-
-  handlePaginationChange = e => {
-    const { value } = e.target;
-    this.setState({
-      pagination: value === "none" ? false : { position: value }
-    });
-  };
-
   render() {
     const state = this.state;
     return (
@@ -124,89 +76,41 @@ class UserList extends React.Component {
         <Card title="列表示例">
           <div className="components-table-demo-control-bar">
             <Form layout="inline">
-              <FormItem label="Bordered">
-                <Switch
-                  checked={state.bordered}
-                  onChange={this.handleToggle("bordered")}
-                />
-              </FormItem>
-              <FormItem label="loading">
-                <Switch
-                  checked={state.loading}
-                  onChange={this.handleToggle("loading")}
-                />
-              </FormItem>
-              <FormItem label="Title">
-                <Switch
-                  checked={!!state.title}
-                  onChange={this.handleTitleChange}
-                />
-              </FormItem>
-              <FormItem label="Column Header">
-                <Switch
-                  checked={!!state.showHeader}
-                  onChange={this.handleHeaderChange}
-                />
-              </FormItem>
-              <FormItem label="Footer">
-                <Switch
-                  checked={!!state.footer}
-                  onChange={this.handleFooterChange}
-                />
-              </FormItem>
-              <FormItem label="Expandable">
-                <Switch
-                  checked={!!state.expandedRowRender}
-                  onChange={this.handleExpandChange}
-                />
-              </FormItem>
-              <FormItem label="Checkbox">
-                <Switch
-                  checked={!!state.rowSelection}
-                  onChange={this.handleRowSelectionChange}
-                />
-              </FormItem>
-              <FormItem label="Fixed Header">
-                <Switch
-                  checked={!!state.scroll}
-                  onChange={this.handleScollChange}
-                />
-              </FormItem>
-              <FormItem label="Has Data">
-                <Switch
-                  checked={!!state.hasData}
-                  onChange={this.handleDataChange}
-                />
-              </FormItem>
-              <FormItem label="Size">
-                <Radio.Group
-                  size="default"
-                  value={state.size}
-                  onChange={this.handleSizeChange}
-                >
-                  <Radio.Button value="default">Default</Radio.Button>
-                  <Radio.Button value="middle">Middle</Radio.Button>
-                  <Radio.Button value="small">Small</Radio.Button>
-                </Radio.Group>
-              </FormItem>
-              <FormItem label="Pagination">
-                <Radio.Group
-                  value={state.pagination ? state.pagination.position : "none"}
-                  onChange={this.handlePaginationChange}
-                >
-                  <Radio.Button value="top">Top</Radio.Button>
-                  <Radio.Button value="bottom">Bottom</Radio.Button>
-                  <Radio.Button value="both">Both</Radio.Button>
-                  <Radio.Button value="none">None</Radio.Button>
-                </Radio.Group>
-              </FormItem>
+              <Search
+                placeholder="请输入ID"
+                onSearch={value => this.onStatusSelectChange(value)}
+                enterButton="搜索"
+                style={{ width: 200 }}
+              />
+              <Select
+                placeholder="内容类别"
+                style={{ width: 120, margin: "0 20px" }}
+                onChange={this.onStatusSelectChange}
+              >
+                <Option key={1} value="经理言论">
+                  经理言论
+                </Option>
+                <Option key={2} value="投研报告">
+                  投研报告
+                </Option>
+                <Option key={3} value="经理采访">
+                  经理采访
+                </Option>
+              </Select>
+              <Button
+                type="primary"
+                style={{ marginLeft: 20 }}
+                onClick={this.handleButtonClick}
+              >
+                重置
+              </Button>
             </Form>
           </div>
           <Divider />
           <Table
             {...this.state}
             columns={columns}
-            dataSource={state.hasData ? data : null}
+            dataSource={this.store.data}
           />
         </Card>
       </div>
