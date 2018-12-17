1052a2f75e918bb02b6e09eae6e6291eda65b70c
yuxz <yuxz@xueqiu.com>
2018-12-17 19:31:56 +0800

    modal 示例

---
+++ src/pages/Example/Modal/index.js
@@ -0,0 +1,88 @@
+import { Row, Button, Input, Col, Card, Divider, Modal, Form } from "antd";
+import { observer } from "mobx-react";
+import store from "./store";
+
+const Search = Input.Search;
+const FormItem = Form.Item;
+
+@observer
+class Info extends React.Component {
+  store = store;
+
+  handleOk = () => {
+    this.toggleVisible()
+  }
+  toggleVisible = () => {
+    this.store.toggleVisible()
+  }
+  changeAge = (e) => {
+    this.store.userInfo.age = e.target.value;
+  }
+  changeName = (e) => {
+    this.store.userInfo.name = e.target.value;
+  }
+  render() {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+
+    return (
+      <div>
+        <Card title='详情页示例'
+          extra={
+            <Button type="primary" onClick={this.toggleVisible}>
+              修改弹窗
+            </Button>
+          }>
+          <div>
+            <Row gutter={24}>
+              <Col span={8}>
+                姓名：{this.store.userInfo.name}
+              </Col>
+              <Col span={8}>
+                年龄：{this.store.userInfo.age}
+              </Col>
+              <Col span={8}>
+                电话：{this.store.userInfo.phone}
+              </Col>
+            </Row>
+            {/* <Divider /> */}
+            <Row gutter={24}>
+              <Col span={8}>
+                住址：{this.store.userInfo.address}
+              </Col>
+              <Col span={8}>
+                描述：{this.store.userInfo.description}
+              </Col>
+            </Row>
+            <Divider />
+          </div>
+        </Card>
+        <Modal
+          title="修改用户信息"
+          visible={this.store.modalVisible}
+          onOk={this.handleOk}
+          onCancel={this.toggleVisible}
+        >
+          <Form>
+            <FormItem {...formItemLayout} label='姓名' >
+              <Input value={this.store.userInfo.name} onChange={this.changeName} />
+            </FormItem>
+            <FormItem {...formItemLayout} label='年龄'>
+              <Input type="number" value={this.store.userInfo.age} onChange={this.changeAge} />
+            </FormItem>
+          </Form>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default Info;
---
+++ src/pages/Example/Modal/store.js
@@ -0,0 +1,27 @@
+import { observable, computed, action } from "mobx";
+import { message } from "antd";
+
+class Store {
+  @observable userInfo = {
+    name: '雪球用户',
+    email: '123456@163.com',
+    age: 34,
+    address: '望京西园一区 411号楼 304',
+    phone: '13412341234',
+    description: '一位不明真相的群众'
+  }
+  @observable modalVisible = false
+
+  @action
+  toggleVisible() {
+    this.modalVisible = !this.modalVisible
+  }
+
+  @action
+  getUser(value) {
+    message.success("查询成功");
+    this.userInfo.name = value
+  }
+}
+
+export default new Store();
