f02e5b5a69427e77bfe79f180bd131c480573b03
yuxz <yuxz@xueqiu.com>
2019-01-16 11:02:45 +0800

    add welcome page

--- src/pages/NoMatch.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function NoMatch() {
-  return <div>欢迎来到雪球CRM</div>;
-}
---
+++ src/pages/NoMatch/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export default function NoMatch() {
+  return (
+    <div className="home-page">
+      <iframe title="welcome" src="/welcome.html" />
+    </div>
+  );
+}
