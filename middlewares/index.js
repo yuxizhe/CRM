74dc935ceff0f92ef08769044006a4d64e08aa83
yuxz <yuxz@xueqiu.com>
2019-01-18 18:10:05 +0800

    add logger

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,12 @@
 // 中间件列表添加在这里
-
+const logger = require('snb-lib-log');
 // 接口代理
 const proxy = require('./proxy');
 // 接口mock
 const mock = require('./mock');
 
 module.exports = (app) => {
+  logger(app);
   mock(app);
   proxy(app);
 };
